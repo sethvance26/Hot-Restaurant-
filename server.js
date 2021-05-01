c6a44770e803c761985a009ce4476b4428288b8d
Seth <sethvance26@gmail.com>
2021-05-01 11:13:35 -0400

    Pushing changes to server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getMaxListeners } = require('node:process');
+// const { getMaxListeners } = require('node:process');
 const path = require('path');
 
 const app = express();
@@ -15,9 +15,19 @@ const customers = [
         email: 'dan@gmail.com',
         phone: '3984712'
         
-    }
+    },
     {
         routeName: "TableTwo",
         id: 394871,
+        email: 'bob@gmail.com',
+        phone: '8045021'
     }
-]
+];
+
+app.get('/', (req, res) => {
+console.log(__dirname);
+ res.sendFile(path.join(__dirname, 'home.html'));
+ 
+});
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
